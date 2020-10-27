7a6a087a11d6384419136607b5fa9af0019720cb
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-27 07:15:54 +0200

    Update path in files_manager

--- FILES/files_manager.py
+++ FILES/files_manager.py
@@ -2,14 +2,14 @@ import os
 
 # Absolute path
 this_file_path = os.path.abspath(__file__)
-print(this_file_path)
+# print(this_file_path)
 BASE_DIR = os.path.dirname(this_file_path)
 ENTIRE_PROJECT_DIR = os.path.dirname(BASE_DIR)
-print("BASE_DIR", BASE_DIR, "ENTIRE_PROJECT_DIR", ENTIRE_PROJECT_DIR)
+# print(BASE_DIR, ENTIRE_PROJECT_DIR)
 
 # email_txt = "templates/email.txt"
 
-email_txt = os.path.join("templates", "email.txt")
+email_txt = os.path.join(BASE_DIR, "templates", "email.txt")
 
 content = ''
 
