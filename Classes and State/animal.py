482f3eef9c9500d28ec5d9f750b7b3869ff424b7
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-28 06:49:13 +0200

    Add templates

--- Classes and State/animal.py
+++ Classes and State/animal.py
@@ -1,8 +1,29 @@
 class Animal:
     noise = "Rrrr"
+    color = "White"
 
     def make_noise(self):
         print(f'I say {self.noise}')
 
+    def get_noise(self):
+        return self.noise
+
     def set_noise(self, new_noise):
         self.noise = new_noise
+        return self.noise
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, new_color):
+        self.color = new_color
+        return self.color
+
+
+class Wolf(Animal):
+    noise = "Grrrr"
+
+
+class BabyWolf(Wolf):
+    color = "Pink"
+
