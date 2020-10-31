e095ea91ab05fe8deef8b2bb723e112cffa2a47b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-31 10:10:43 +0200

    Update send_email: with class options

--- Classes and State/send_mail.py
+++ Classes and State/send_mail.py
@@ -5,16 +5,21 @@ from email.mime.multipart import MIMEMultipart
 from templates import Template
 # Environment variables
 username = 'honeydummyams@gmail.com'
-password = ''
+password = '^516YWCAW&%spj3q'
 
 
 class Emailer():
     subject = ''
+    context = {}
     to_emails = []
     has_html = False
+    test_send = False
     from_email = 'Honney Bunny <honeydummyams@gmail.com>'
+    template_html = None
+    template_name = None
 
-    def __init__(self, subject='', template_name=None, context={}, template_html=None, to_emails=None):
+    def __init__(self, subject='', template_name=None, context={},
+                 template_html=None, to_emails=None, test_send=False):
         if template_name == None and template_html == None:
             raise Exception("You must set a template")
         assert isinstance(to_emails, list)
@@ -22,6 +27,10 @@ class Emailer():
         self.subject = subject
         if template_html != None:
             self.has_html = True
+            self.template_html = template_html
+        self.template_name = template_name
+        self.context = context
+        self.test_send = test_send
 
     def format_msg(self):
         msg = MIMEMultipart('alternative')
@@ -31,26 +40,30 @@ class Emailer():
 
         if self.template_name != None:
             tmpl_str = Template(template_name=self.template_name, context=self.context)
+            txt_part = MIMEText(tmpl_str.render(), 'plain')
+            print(txt_part)
+            msg.attach(txt_part)
 
-        # txt_part = MIMEText(text, 'plain')
-        # msg.attach(txt_part)
-        # if html != None:
-        #     html_part = MIMEText(html, 'html')
-        #     msg.attach(html_part)
+        if self.template_html != None:
+            tmpl_str = Template(template_name=self.template_html, context=self.context)
+            html_part = MIMEText(tmpl_str.render(), 'html')
+            print(html_part)
+            msg.attach(html_part)
+
         msg_str = msg.as_string()
         return msg_str
 
     def send_mail(self):
         msg = self.format_msg()
-
         did_send = False
-        with smtplib.SMTP(host='smtp.gmail.com', port=587) as server:
-            server.ehlo()
-            server.starttls()
-            server.login(username, password)
-            try:
-                server.sendmail(from_email, to_emails, msg_str)
-                did_send = True
-            except:
-                did_send = False
+        if not test.test_send:
+            with smtplib.SMTP(host='smtp.gmail.com', port=587) as server:
+                server.ehlo()
+                server.starttls()
+                server.login(username, password)
+                try:
+                    server.sendmail(from_email, to_emails, msg_str)
+                    did_send = True
+                except:
+                    did_send = False
         return did_send
--- FILES/README.md
+++ FILES/README.md
@@ -2,3 +2,7 @@
 Python lessons - Files - Create, Read & Download
     - python "D:\DJANGO\octo-funicular\FILES\files_manager.py"
      
+     
+ - obj = Emailer(subject="Hello My World", context={"name":"DarishkaAMS"}, template_name="hello.txt",
+ to_emails=["d.nikolaienko@yahoo.co.uk"])
+
