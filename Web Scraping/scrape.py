22c5bf8f3dc2d7991eecec5b65d54df7e9387a1d
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-01 14:37:18 +0200

    'Add'

--- Web Scraping/scrape.py
+++ Web Scraping/scrape.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+from requests_html import HTML
 
 now = datetime.datetime.now()
 year = now.year
@@ -10,17 +11,20 @@ def url_to_txt(url, filename='world.html', save=False):
     if r.status_code == 200:
         html_text = r.text
         html_text = html_text.encode('utf-8').decode('ascii', 'ignore') # to en/decode
-        # if save:
-        with open(f"world-{year}.html", 'w') as f:
-            f.write(html_text)
+        if save:
+            with open(f'world-{year}.html', 'w') as f:
+                f.write(html_text)
         return html_text
     return ""
 
 
 url = "https://www.boxofficemojo.com/year/world/"
-# url = "https://www.facebook.com/"
 
-url_to_txt(url)
-#html_text =
+html_text = url_to_txt(url)
 
+r_html = HTML(html=html_text)
+table_class = '.imdb-scroll-table'
+# "a-section imdb-scroll-table mojo-gutter imdb-scroll-table-styles"
+r_table = r_html.find(table_class)
 
+print(r_table)
