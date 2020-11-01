a1c711fe35e1b6e79acba020b5bfcef1dec93946
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-01 13:56:55 +0200

    Add url_to_file @ Web Scraping

---
+++ Web Scraping/scrape.py
@@ -0,0 +1,22 @@
+import datetime
+import requests
+
+now = datetime.datetime.now()
+year = now.year
+
+
+def url_to_file(url, filename='world.html'):
+    r = requests.get(url)
+    if r.status_code == 200:
+        html_text = r.text
+        with open(f"world-{year}.html", 'w') as f:
+            f.write(html_text)
+        return html_text
+    return ""
+
+# url = "https://www.boxofficemojo.com/year/world/"
+url = "https://www.facebook.com/"
+
+url_to_file(url)
+
+
