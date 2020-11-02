2354dafa0f0081c0b323ef847eca3e00e0345a45
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-02 07:51:24 +0200

    Update the query + data formating wiht json

--- REST API Service/connect.py
+++ REST API Service/connect.py
@@ -1,5 +1,6 @@
 import requests
 api_key = "9db581b66af3875cd0f60398f7e45671"
+api_key_v4 = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZGI1ODFiNjZhZjM4NzVjZDBmNjAzOThmN2U0NTY3MSIsInN1YiI6IjVmOTc5ZDMyZDJmNWI1MDAzYTdlYjE2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H1mCVcN4474KHzCW6nLmGB9gNz073JdXw4T8pwuo9xk"
 
 # HTTP Requests
 # What's our endpoint // a url?
@@ -11,12 +12,36 @@ Endpoint
 https://api.themoviedb.org/3/movie/550?api_key=9db581b66af3875cd0f60398f7e45671
 '''
 
+# Using v3
 movie_id = 500
 api_version = 3
 api_base_url = f'https://api.themoviedb.org/{api_version}'
 endpoint_path = f'/movie/{movie_id}'
 endpoint = f'{api_base_url}{endpoint_path}?api_key={api_key}'
+# print(endpoint)
+# r = requests.get(endpoint) # json={"api_key": api_key})
+# print(r.status_code)
+# print(r.text)
+
+# Using v4
+movie_id = 501
+api_version = 4
+api_base_url = f'https://api.themoviedb.org/{api_version}'
+endpoint_path = f'/movie/{movie_id}'
+endpoint = f'{api_base_url}{endpoint_path}' # ?api_key={api_key}'
+# print(endpoint)
+# headers = {
+#     'Authorization': f'Bearer {api_key_v4}',
+#     'Content-Type': 'application/json;charset=utf-8'
+# }
+# r = requests.get(endpoint, headers=headers) # json={"api_key": api_key})
+# print(r.status_code)
+# print(r.text)
+
+api_base_url = f'https://api.themoviedb.org/{api_version}'
+endpoint_path = f'/search/movie'
+endpoint = f'{api_base_url}{endpoint_path}?api_key={api_key}&' \
+           f'query=Endgame'
 print(endpoint)
-r = requests.get(endpoint) # json={"api_key": api_key})
-print(r.status_code)
-print(r.text)
+r = requests.get(endpoint)
+print(r.json())
