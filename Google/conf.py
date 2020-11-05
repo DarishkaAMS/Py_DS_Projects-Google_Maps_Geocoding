84130f7eb82aceff1f93252cd25196d37e9c5f59
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-05 05:16:33 +0200

    Add click_to_follow

--- Google/conf.py
+++ Google/conf.py
@@ -1,3 +1,5 @@
 
 INSTA_USERNAME = 'honeydummyams'
-INSTA_PASSWORD = ''
+INSTA_PASSWORD = '!6T500lJF0knioK9'
+
+
--- Google/insta.py
+++ Google/insta.py
@@ -21,5 +21,32 @@ submit_btl_el.click()
 
 body_el = browser.find_element_by_css_selector('body')
 html_text = body_el.get_attribute('innerHTML')
+# print(html_text)
 
-print(html_text)
+'''
+<button class="_5f5mN       jIbKX  _6VtSN     yZn4P   ">Follow</button>
+'''
+
+# browser.find_elements_by_css_selector('button')
+
+# Using xpath
+# my_button_xpath = '//button'
+# browser.find_elements_by_css_selector(my_button_xpath)
+
+
+def click_to_follow(browser):
+    # my_follow_btn_xpath = "//a[contains(text(), 'Follow')][not(contains(text(), 'Following'))]"
+    # my_follow_btn_xpath = "//*[contains(text(), 'Follow')][not(contains(text(), 'Following'))]"
+    my_follow_btn_xpath = "//button[contains(text(), 'Follow')][not(contains(text(), 'Following'))]"
+    follow_btn_elments = browser.find_elements_by_xpath(my_follow_btn_xpath)
+    for btn in follow_btn_elments:
+        time.sleep(2)  # self-throttle
+        try:
+            btn.click()
+        except:
+            pass
+
+new_user_url = 'https://www.instagram.com/ted/'
+browser.get(new_user_url)
+click_to_follow(browser)
+
